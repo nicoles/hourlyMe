78d4bcce96d261b109b5c25743ddff6e348597a5
binx <rachelbinx@gmail.com>
2011-09-23 16:48:28 -0700

    added larger images and dummy pages for the other views

---
+++ hourlyMe/AnalyticsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnalyticsViewController.h
+//  hourlyMe
+//
+//  Created by Rachel Binx on 9/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnalyticsViewController : UIViewController
+
+@end
